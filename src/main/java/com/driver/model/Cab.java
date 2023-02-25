3aa80e9da0c83bbd05f033708d411b808cbb9440
Dabloo Gupta <dabloogupta67@gmail.com>
2023-02-25 15:14:09 +0530

    uber2

--- src/main/java/com/driver/model/Cab.java
+++ src/main/java/com/driver/model/Cab.java
@@ -47,6 +47,9 @@ public class Cab {
     public boolean isAvailable() {
         return available;
     }
+    public boolean getAvailable() {
+        return available;
+    }
 
     public void setAvailable(boolean available) {
         this.available = available;
